261674d3352da3de47161aa5a9b81c834abcce5f
Clay Branch <clay@movementstrategy.com>
2012-09-19 12:57:03 -0400

    added test file

---
+++ php_report_builder.php
@@ -0,0 +1,22 @@
+<?php
+
+	class ReportBuilder {
+		
+		static $requiredParams = array(
+			'x',
+			'y',
+		);
+		
+		static $requiredXAndY = array(
+			'build',
+			'gather',
+			'label',
+			'sort',
+		);
+		
+		
+		static function build() {
+			
+		}
+		
+	}
